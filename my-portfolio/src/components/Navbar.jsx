17b03e1d27dab93a140cfa639232abd4ce55bebf
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2023-11-30 14:29:51 +0300

    Add animation when toggling display of navbar on small devices

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-scroll";
 import { motion } from "framer-motion";
+
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const links = [
@@ -26,6 +27,7 @@ const Navbar = () => {
       link: "contact",
     },
   ];
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-8 py-12 bg-black text-white fixed z-10">
       <div>
@@ -51,21 +53,26 @@ const Navbar = () => {
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
             <Link to={link} smooth duration={500}>
-              {" "}
               {link}
             </Link>
           </li>
         ))}
       </motion.ul>
 
-      <div
+      <motion.div
         onClick={() => setNav(!nav)}
         className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden "
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
+      </motion.div>
+
       {nav && (
-        <motion.ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <motion.ul
+          initial={{ opacity: 0, x: -200 }} // Adjusted the initial properties here
+          animate={{ x: 0, opacity: 1 }} // Added opacity animation for a smoother transition
+          transition={{ duration: 0.3, type: "tween" }}
+          className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500"
+        >
           {links.map(({ id, link }) => (
             <li
               key={id}
@@ -77,7 +84,6 @@ const Navbar = () => {
                 smooth
                 duration={500}
               >
-                {" "}
                 {link}
               </Link>
             </li>
