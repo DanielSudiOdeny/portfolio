1bc6f1cf5314cf9d1f253d43346d1e19b402679c
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2023-12-20 12:21:46 +0300

    Add titles to skills sections

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -10,40 +10,41 @@ import typescript from "../assets/typescript.png";
 import postgress from "../assets/postgress.png";
 import django from "../assets/django.png";
 import postman from "../assets/postman.png";
+import redux from "../assets/redux.png";
 const Experience = () => {
   const techstack = [
     {
       id: 1,
       src: html,
-      // title: "HTML",
+      title: "HTML",
       style: "shadow-orange-500",
       textColor: "hover:text-orange-500",
     },
     {
       id: 2,
       src: css,
-      // title: "CSS",
+      title: "CSS",
       style: "shadow-sky-600",
       textColor: "hover:text-sky-600",
     },
     {
       id: 3,
       src: javascript,
-      // title: "Javascript",
+      title: "Javascript",
       style: "shadow-yellow-500",
       textColor: "hover:text-yellow-500",
     },
     {
       id: 4,
       src: python,
-      // title: "Python",
+      title: "Python",
       style: "shadow-yellow-600",
       textColor: "hover:text-yellow-600",
     },
     {
       id: 5,
       src: react,
-      // title: "React",
+      title: "React",
       style: "shadow-blue-700",
       textColor: "hover:text-blue-700",
     },
@@ -51,44 +52,51 @@ const Experience = () => {
     {
       id: 6,
       src: tailwindcss,
-      // title: "Tailwindcss",
+      title: "Tailwindcss",
       style: "shadow-blue-400",
       textColor: "hover:text-blue-400",
     },
     {
       id: 7,
       src: flask,
-      // title: "Flask",
+      title: "Flask",
       style: "shadow-white",
       textColor: "hover:text-white",
     },
     {
       id: 8,
       src: typescript,
-      // title: "Flask",
+      title: "TypeScript",
       style: "shadow-sky-900",
-      textColor: "hover:text-white",
+      textColor: "hover:text-sky-600",
     },
     {
       id: 9,
       src: postgress,
-      // title: "Flask",
+      title: "Postgresql",
       style: "shadow-cyan-500",
-      textColor: "hover:text-white",
+      textColor: "hover:text-cyan-500",
     },
     {
       id: 10,
       src: django,
-      // title: "Flask",
+      title: "Django",
       style: "shadow-green-900",
       textColor: "hover:text-lime-600",
     },
     {
       id: 10,
       src: postman,
-      // title: "Flask",
+      title: "Postman",
       style: "shadow-amber-900",
       textColor: "hover:text-amber-900",
+    },
+    {
+      id: 11,
+      src: redux,
+      title: "Redux",
+      style: "shadow-purple-700",
+      textColor: "hover:text-purple-700",
     },
   ];
   return (
@@ -98,21 +106,25 @@ const Experience = () => {
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center text-white">
         <div className="flex flex-col items-center">
-          <p className="text-4xl font-bold tracking-[20px] text-gray-500 inline">
+          <p className="text-4xl  font-bold tracking-[20px] text-gray-500 inline">
             Skills
           </p>
           {/* <p className="py-4 tracking-[3px]">
             These are the technologies I've worked with
           </p> */}
         </div>
-        <div className="w-full grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-10 text-center place-items-center py-8 px-6 sm:px-0">
+        <div className="w-full grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 gap-10 text-center place-items-center py-10 md:py-8 sm:px-0">
           {techstack.map(({ id, src, title, style, textColor }) => (
             <div
               key={id}
-              className={`shadow-md flex flex-col justify-center items-center rounded-full w-20 hover:scale-110 hover:bg-grey-100 duration-500 py-2 h-20 ${style} ${textColor}`}
+              className={`shadow-md flex flex-col justify-center items-center md:rounded-3xl rounded-2xl  md:w-28 hover:scale-110 hover:bg-grey-100 duration-500 py-2 h-20 w-20 text-sm md:h-28 ${style} ${textColor}`}
             >
-              <img src={src} alt="" className="w-10 mx-auto object-cover" />
-              <p className="mt-4">{title}</p>
+              <img
+                src={src}
+                alt=""
+                className="md:w-10 w-8 mx-auto object-cover"
+              />
+              <p className="mt-4 text-sm">{title}</p>
             </div>
           ))}
         </div>
